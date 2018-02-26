d8c6fce80bd60bd6a80e9be088c278e5d9508f69
Kevin Lin <kevinlinxp@gmail.com>
2018-02-27 02:58:37 +1030

    run antlr stuff in unit test

--- src/test/kotlin/com/kevinlinxp/sublimeSnippetsSupport/SublimeSnippetContentProcessorTest.kt
+++ src/test/kotlin/com/kevinlinxp/sublimeSnippetsSupport/SublimeSnippetContentProcessorTest.kt
@@ -1,6 +1,8 @@
 package com.kevinlinxp.sublimeSnippetsSupport
 
 import com.intellij.util.loadElement
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.InputStream
@@ -25,8 +27,6 @@ class SublimeSnippetContentProcessorTest {
     @Test
     fun testSublimeSnippetContentProcessor() {
 
-        val str = "$"
-
         list.forEach {
             val sublimeSnippetStream = SublimeSnippetProcessor::class.java.getResourceAsStream("$it.sublime-snippet")
             val element = loadElement(sublimeSnippetStream)
@@ -39,6 +39,17 @@ class SublimeSnippetContentProcessorTest {
             assertEquals("$it failed test.", liveTemplate, processor.liveTemplate)
         }
 
+    }
+
+    @Test
+    fun testANTLR() {
+        val inputStream = ANTLRInputStream("Snippet\${1:placeholder}")
+        val lexer = com.kevinlinxp.sublimeSnippetsSupport.SublimeSnippetLexer(inputStream)
+        val commonTokenStream = CommonTokenStream(lexer)
+        val parser = com.kevinlinxp.sublimeSnippetsSupport.SublimeSnippetParser(commonTokenStream)
+
+        val snippet = parser.snippet()
+        snippet.content()
     }
 
 
