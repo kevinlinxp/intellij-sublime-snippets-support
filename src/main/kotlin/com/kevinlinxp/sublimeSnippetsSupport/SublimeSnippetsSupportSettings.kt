4d7c33f97ba6fd93df34dc01a8fb4f7b6ef50905
Kevin Lin <kevinlinxp@gmail.com>
2019-09-29 21:22:46 +1000

    Add default sublime snippets root folder for Linux system and be more defensive to IO Exceptions.

--- src/main/kotlin/com/kevinlinxp/sublimeSnippetsSupport/SublimeSnippetsSupportSettings.kt
+++ src/main/kotlin/com/kevinlinxp/sublimeSnippetsSupport/SublimeSnippetsSupportSettings.kt
@@ -2,6 +2,7 @@ package com.kevinlinxp.sublimeSnippetsSupport
 
 import com.intellij.codeInsight.template.Template
 import com.intellij.codeInsight.template.impl.TemplateSettings
+import com.intellij.ide.plugins.PluginManager
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.components.State
@@ -9,6 +10,7 @@ import com.intellij.openapi.components.Storage
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.util.io.exists
 import com.intellij.util.io.isDirectory
+import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -36,7 +38,8 @@ class SublimeSnippetsSupportSettings : PersistentStateComponent<SublimeSnippetsS
             return when {
                 SystemInfo.isMac -> System.getProperty("user.home") + "/Library/Application Support/Sublime Text 3/Packages"
                 SystemInfo.isWindows -> System.getProperty("user.home") + "/AppData/Roaming/Sublime Text 3/Packages"
-                else -> System.getProperty("user.home")
+                SystemInfo.isLinux -> System.getProperty("user.home") + "/.config/sublime-text-3/Packages"
+                else -> ""
             }
         }
     }
@@ -60,16 +63,28 @@ class SublimeSnippetsSupportSettings : PersistentStateComponent<SublimeSnippetsS
                 .forEach(templateSettings::removeTemplate)
 
 
-        val rootDir = Paths.get(this.state.sublimeSnippetsRoot) ?: return
-        if (!rootDir.exists() || !rootDir.isDirectory()) return
+        val sublimeSnippetsRoot = this.state.sublimeSnippetsRoot
+        if (sublimeSnippetsRoot.isBlank()) return
+        try {
+            val rootDir = Paths.get(sublimeSnippetsRoot) ?: return
+            if (!rootDir.exists() || !rootDir.isDirectory()) {
+                PluginManager.getLogger().error("Invalid root directory. ${errorMessage(sublimeSnippetsRoot)}")
+                return
+            }
 
-        Files.walk(rootDir)
-                .filter { !it.isDirectory() && it.toString().endsWith(".sublime-snippet") }
-                .map(this::toTemplate)
-                .collect(Collectors.toList())
-                .filterNotNull()
-                .forEach(templateSettings::addTemplate)
+            Files.walk(rootDir)
+                    .filter { !it.isDirectory() && it.toString().endsWith(".sublime-snippet") }
+                    .map(this::toTemplate)
+                    .collect(Collectors.toList())
+                    .filterNotNull()
+                    .forEach(templateSettings::addTemplate)
+        } catch (e: Exception) {
+            PluginManager.getLogger().error("Exception (${e.message}) occurred while parsing snippets. ${errorMessage(sublimeSnippetsRoot)}", e)
+        }
     }
+
+    private fun errorMessage(currentRoot: String) =
+            "Please update the sublime snippets root directory (current value: $currentRoot) in settings and try again."
 
     private fun toTemplate(sublimeSnippetFile: Path): Template? {
         return SublimeSnippetProcessor.create(sublimeSnippetFile).getLiveTemplate()
